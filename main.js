0d511d8c89c3e6f8899dc6969d17747039ae9225
Valeria Leza Sanchez <valerialeza.s@gmail.com>
2023-03-19 19:58:38 -0600

    Se actualizo el proyecto de acuerdo a los requerimientos funcionales

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let = categoria = document.getElementById('categoria');
 let = estatus = document.getElementById('estatus');
 let opcion = ''
 
+
+
 btnCrear.addEventListener('click', ()=>{
     nombreProducto.value=''
     comentarios.value=''
@@ -32,7 +34,7 @@ const mostrar=(productos) =>{
                             <td>${producto.Descripcion}</td>
                             <td>${producto.Categoria}</td>
                             <td>${producto.Estatus}</td>
-                            <td class="text-center"><button id="btnEditar" type="button" class="btn btn-primary btnEditar">Editar</button><button id="btnBorrar" type="button" class="btn btn-danger btnBorrar">Borrar</button></td>
+                            <td class="text-center"><button id="btnEditar" type="button" class="btn btn-primary btnEditar">Editar</button></td>
                         </tr>
         `
     })
@@ -53,7 +55,7 @@ const on = (element, event, selector, handler) => {
 }
 
 //BORRAR
-on(document, 'click', '.btnBorrar', e =>{
+/* on(document, 'click', '.btnBorrar', e =>{
     const fila = e.target.parentNode.parentNode
     const id = fila.firstElementChild.innerHTML
     console.log(id);
@@ -62,7 +64,7 @@ on(document, 'click', '.btnBorrar', e =>{
     })
     .then( res=>res.json())
     .then(()=> location.reload())
-})
+}) */
 
 //EDITAR
 let idForm = 0
@@ -85,8 +87,83 @@ on(document, 'click', '.btnEditar', e =>{
 
 formProducto.addEventListener('submit', (e)=>{
     e.preventDefault()
+    
     if(opcion=='crear'){
-        fetch(url, {
+        //  VALIDACIONES DE FORMULARIO
+    const flag = {
+    nombreProducto: false,
+    comentarios: false,
+    descripcion: false,
+    categoria: false,
+    estatus: false
+}
+
+//  Validacion nombreProducto
+    nombreProducto.classList.remove("is-invalid");
+    nombreProducto.classList.add("is-valid");
+ 
+    if ((nombreProducto.value.length >= 3) && (nombreProducto.value.length < 50) && !(nombreProducto.value.trim() == "") && (nombreProducto.value[0] != " ")) {
+        nombreProducto.classList.add("is-valid"); 
+        flag.nombreProducto = true
+    }
+    else{  
+         nombreProducto.classList.add("is-invalid");
+         flag.nombreProducto = false
+    }
+//  Validacion comentarios
+    comentarios.classList.remove("is-invalid");
+    comentarios.classList.add("is-valid");
+ 
+    if ((comentarios.value.length >= 3) && (comentarios.value.length < 100) && !(comentarios.value.trim() == "") && (comentarios.value[0] != " ")) {
+        comentarios.classList.add("is-valid"); 
+        flag.comentarios = true
+    }
+    else{  
+         comentarios.classList.add("is-invalid");
+         flag.comentarios = false
+    }
+
+    //  Validacion descripcion
+    descripcion.classList.remove("is-invalid");
+    descripcion.classList.add("is-valid");
+ 
+    if ((descripcion.value.length >= 3) && (descripcion.value.length < 200) && !(descripcion.value.trim() == "") && (descripcion.value[0] != " ")) {
+        descripcion.classList.add("is-valid"); 
+        flag.descripcion = true
+    }
+    else{  
+         descripcion.classList.add("is-invalid");
+         flag.descripcion = false
+    }
+
+    //  Validacion categoria
+    categoria.classList.remove("is-invalid");
+    categoria.classList.add("is-valid");
+ 
+    if (categoria.value.length>0 && categoria.value>0) {
+        categoria.classList.add("is-valid"); 
+        flag.categoria = true
+    }
+    else{  
+         categoria.classList.add("is-invalid");
+         flag.categoria = false
+    }
+
+    //  Validacion estatus
+    estatus.classList.remove("is-invalid");
+    estatus.classList.add("is-valid");
+ 
+    if (estatus.value.length>0) {
+        estatus.classList.add("is-valid"); 
+        flag.estatus = true
+    }
+    else{  
+         estatus.classList.add("is-invalid");
+         flag.estatus = false
+    }
+
+    if (flag.nombreProducto && flag.comentarios && flag.descripcion && flag.categoria && flag.estatus){
+            fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type':'application/json'
@@ -100,11 +177,20 @@ formProducto.addEventListener('submit', (e)=>{
             })
         })
         .then(response => response.json())
+        .then(()=> location.reload())
         .then(data => {
             const nuevoProducto = []
             nuevoProducto.push(data)
             mostrar(nuevoProducto)
         })
+        nombreProducto.classList.remove("is-valid")
+        comentarios.classList.remove("is-valid")
+        descripcion.classList.remove("is-valid")
+        categoria.classList.remove("is-valid")
+        estatus.classList.remove("is-valid")
+        modalProducto.hide()
+    }
+        
     }
     if(opcion=='editar'){
         fetch(url+idForm, {
@@ -123,5 +209,5 @@ formProducto.addEventListener('submit', (e)=>{
         .then(response => response.json())
         .then(()=> location.reload())
     }
-    modalProducto.hide()
+    
 })
